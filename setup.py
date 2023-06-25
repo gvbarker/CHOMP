1367b3404f37ea075e9fe57398448161e26cb6cd
gvbarker <gvbarker23@wabash.edu>
2023-06-25 05:11:01 -0400

    removed testing files, restructured directories, and cleaned up styling in chomp.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='chomp',
+    version='1.1.2',
+    description='v2 BASIC Tokenizer',
+    url='https://github.com/gvbarker/CHOMP',
+    author='Giancarlo Barker',
+    author_email='jonzomb80@gmail.com',
+    license='MIT',
+    packages=['chomp'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
