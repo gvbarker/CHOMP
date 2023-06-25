29c9996e1c9dc5d9350cd714db305c71730fd6cb
gvbarker <gvbarker23@wabash.edu>
2023-06-25 02:59:01 -0400

    Added functionality for verbose and overwrite CLI flags

--- src/chomp.py
+++ src/chomp.py
@@ -1,22 +1,30 @@
-import argparse,sys,os,struct
+import argparse,os,struct
 class tokenizer():
     def __init__(self, infile, outfile, flags):
         self.flags = flags
         self.memoryEntryPoint, self.memoryExitPoint = 0x0801, 0x0000
-        self.overwrite = False
+
         try:
+            if (self.flags['v']):
+                print("CHOMP: Validating input filepath...")
             assert(self.__validateInFile(infile)), "\nCHOMP: Something went wrong. Please check your input filepath.\n"
         except Exception as e:
             print(e)
             exit()         
+        
         try:
+            if (self.flags['v']):
+                print("CHOMP: Validating output filepath...")
             assert(self.__validateOutFile(outfile)), "\nCHOMP: Something went wrong. Please check your output filepath.\n"
         except Exception as e:
             print(e)
             exit()
+        
         self.infile, self.outfile = os.path.join(os.getcwd(),infile), os.path.join(os.getcwd(), outfile)
         self.originalContent = self.__fileContentSplitter()
         self.tokenizedContent = []
+        self.lineTokens = []
+        
         self.v2List = {"END": 0x80,"FOR": 0x81,"NEXT": 0x82,
                         "DATA": 0x83,"INPUT#": 0x84,"INPUT": 0x85,"DIM": 0x86,"READ": 0x87,
                         "LET": 0x88,"GOTO": 0x89,"RUN": 0x8A,"IF": 0x8B,"RESTORE": 0x8C,
@@ -33,6 +41,7 @@ class tokenizer():
                         "ATN": 0xC1,"PEEK": 0xC2,"LEN": 0xC3,"STR$": 0xC4,"VAL": 0xC5,
                         "ASC": 0xC6,"CHR$": 0xC7,"LEFT$": 0xC8,"RIGHT$": 0xC9,"MID$": 0xCA,
                         "GO": 0xCB,"INT": 0xB5} 
+        
         self.keywords= list(self.v2List.keys())
         self.tokens= list(self.v2List.values())
 
@@ -40,21 +49,25 @@ class tokenizer():
         path = os.path.join(os.getcwd(), file)
         correctPathAndFile = (os.path.exists(path) and os.path.isfile(path))
         correctExt = (file[-4:] == ".bas")
+        
         if(correctExt and correctPathAndFile):
             return True
+        
         return False
 
     def __validateOutFile(self,file):
         res = ""
         path = os.path.join(os.getcwd(), file)
-        if (os.path.exists(path) and not self.overwrite):
+        if (os.path.exists(path) and not self.flags['o']):
             while (not res.upper() == 'Y' and not res.upper() == "N"):
                 res = input("\nCHOMP: The output file currently exists. Would you like to overwrite it? (Y/N)  ")
                 print()
+            
             if (res.upper() == 'Y'):
                 return True
             print("CHOMP: Please enter a new output file.\n")
             exit()
+
         return True
 
     def __fileContentSplitter(self):
@@ -62,15 +75,30 @@ class tokenizer():
         contents = file.readlines()
         return contents
 
-
     def tokenize(self):
         for i in range(len(self.originalContent)):
             if(not self.originalContent[i].strip()):
                 continue
+            
+            if(self.flags['v']):
+                print("CHOMP: Crunching line:\t" + self.originalContent[i].strip())
+            
             lineNum,lineContent = self.__stripLineNumber(self.originalContent[i], i)
             lineContent = self.__tokenizeLine(lineContent)
+            
+            if(self.flags['v']):
+                print("CHOMP: Tokens found:\t", self.lineTokens)
+            self.lineTokens = []
             self.tokenizedContent.append((lineNum,lineContent))
+
+        if(self.flags['v']):
+            print("CHOMP: Tokenization successful, writing to output file...")
+
         self.__writeToOuputFile()
+
+        if(self.flags['v']):
+            print("CHOMP: Write successful")
+
 
     def __tokenizeLine(self, line):
         line = line.upper()
@@ -78,32 +106,46 @@ class tokenizer():
         tokenizedLine = []
 
         if ("REM" in noQuotedMaterial):
+            if(self.flags['v']):
+                print("CHOMP: Remarked line, continuing...")
             commentLine = self.__convertToList(noQuotedMaterial.replace("REM",""))
             tokenizedLine.extend([0x8F])
             tokenizedLine.extend(commentLine)
+            self.lineTokens.append("REM")
             return tokenizedLine
+        
         if (':' in noQuotedMaterial):
+            if(self.flags['v']):
+                print("CHOMP: Multiple commands in line, splitting...")
+            
             split = line.index(':')
             left, right = line[0:split], line[split+1:]
             splitLeft, splitRight = self.__tokenizeLine(left), self.__tokenizeLine(right)
+
             tokenizedLine.extend(splitLeft)
             tokenizedLine.extend([ord(":")])
             tokenizedLine.extend(splitRight)
+
             tokenizedLine = [i for i in tokenizedLine if i!='']
             return tokenizedLine
 
         for i in range(len(self.keywords)):
-            currKeyword = self.keywords[i] 
+            currKeyword = self.keywords[i]
+
             if currKeyword in noQuotedMaterial:
                 split = line.index(currKeyword)
+                self.lineTokens.append(currKeyword)
+                
                 left, right = line[0:split], line[split+len(currKeyword):]
                 tokeLeft, tokeRight = self.__tokenizeLine(left), self.__tokenizeLine(right)
+
                 tokenizedLine.extend(tokeLeft)
-                print(currKeyword, hex(self.v2List[currKeyword]))
                 tokenizedLine.extend([self.v2List[currKeyword]])
                 tokenizedLine.extend(tokeRight)
+
                 tokenizedLine = [i for i in tokenizedLine if i!='']
                 return tokenizedLine
+            
         return self.__convertToList(line)
 
     def __writeToOuputFile(self):
@@ -112,6 +154,7 @@ class tokenizer():
         entryLocation,exitLocation = struct.pack('<H', entry), struct.pack('<H', exit)
         location = entry
         out.write(entryLocation)
+
         for line in self.tokenizedContent:
             binLine = self.__convertLineToBinary(line)
             location = 2 + location + len(binLine)
@@ -119,16 +162,19 @@ class tokenizer():
             out.write(binlocation)
             for i in binLine:
                 out.write(i)
+
         out.write(exitLocation)
 
     def __convertLineToBinary(self, lineTup):
         lineNumber, lineContent = lineTup[0], lineTup[1]
         binLineNumber, binLineContent = struct.pack('<H', int(lineNumber)), []
+
         for i in range(len(lineContent)):
             if(type(lineContent[i]) == str):
                 binLineContent.append(struct.pack('B', ord(lineContent[i])))
                 continue
             binLineContent.append(struct.pack('B', (lineContent[i])))
+
         binLineContent.insert(0,struct.pack('B',binLineNumber[1]))
         binLineContent.insert(0,struct.pack('B',binLineNumber[0]))
         binLineContent.append(b'\x00')
@@ -137,31 +183,39 @@ class tokenizer():
     def __stripLineNumber(self, line, contentLineNumber):
         newline,num = '',''
         split = 0
+
         while (line[split].isnumeric()):
             num += line[split]
             split +=1    
         newline+=line[split:]
+
         if(not num):
             print("\nCHOMP: Something went wrong. There is no line number present on line: " + str(contentLineNumber+1) + "\n")
             exit()
+
         newline = newline.strip()
         return (num, newline)
             
     def __removeQuotedMaterial(self,line):
         flag = False
         newstr = ''
+
         for char in line:
             if (not flag and not char == '\"'):
                 newstr += char
+
             if(char=='\"'):
                 flag = not flag 
+
         return newstr                
 
     def __convertToList(self, line):
         lineaslist = []
         lineaslist[:0]=line
+
         for i in range(len(line)):
             lineaslist[i] = line[i]
+
         return lineaslist
 
 if __name__ == "__main__":
@@ -171,5 +225,7 @@ if __name__ == "__main__":
     parser.add_argument("-o","--overwrite", help="Overwrite output file, if it exists", action='store_true')
     parser.add_argument("-v","--verbose", help="Display tokenizer steps", action='store_true')
     args=parser.parse_args()
+
     t = tokenizer(args.inpath, args.outpath, {"o":args.overwrite, 'v':args.verbose})
+    t.tokenize()
     
